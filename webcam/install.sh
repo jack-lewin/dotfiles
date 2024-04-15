7d49dd077a2425432b942cfe2d05be0837bc4b1f
Jack Lewin <14926880+jack-lewin@users.noreply.github.com>
2024-04-15 13:54:02 +0100

    Remove OBS config

--- webcam/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-source $DOTFILES_ROOT/script/internal/link_file.sh
-
-dst="$HOME/Library/LaunchAgents/start-virtual-webcam.plist"
-
-link_file ./start-virtual-webcam.plist "$dst"
-
-launchctl load "$dst"
